ce5b26283e60d6630f948326326c6580bd87d7f6
devtweetz <40036998+devtweetz@users.noreply.github.com>
2018-06-13 20:45:10 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -62,24 +62,26 @@
 					offset: $header.outerHeight()
 				});
 		// Animations
-		var $window = $(window);
-		var $elem = $(".animation")
+		var inView = false;
 
-		    function isScrolledIntoView($elem, $window) {
-			var docViewTop = $window.scrollTop();
-			var docViewBottom = docViewTop + $window.height();
+		function isScrolledIntoView(elem)
+		{
+		    var docViewTop = $(window).scrollTop();
+		    var docViewBottom = docViewTop + $(window).height();
 
-			var elemTop = $elem.offset().top;
-			var elemBottom = elemTop + $elem.height();
+		    var elemTop = $(elem).offset().top;
+		    var elemBottom = elemTop + $(elem).height();
 
-			return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
-		    }
-
-
-		$(document).on("scroll", function () {
-		    if (isScrolledIntoView($elem, $window)) {
-			$elem.addClass("animate")
-			console.log("now you see me");
+		    return ((elemTop <= docViewBottom) && (elemBottom >= docViewTop));
+		}
+		
+		$(window).scroll(function() {
+		    if (isScrolledIntoView('#canvas')) {
+			if (inView) { return; }
+			inView = true;
+			new Chart(document.getElementById("canvas").getContext("2d")).Pie(data);
+		    } else {
+			inView = false;  
 		    }
 		});
 
