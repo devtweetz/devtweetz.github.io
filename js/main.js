03de944bebb102ad195dcc11fb95f15ebd9d1524
devtweetz <40036998+devtweetz@users.noreply.github.com>
2018-06-13 20:41:53 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -61,6 +61,27 @@
 					speed: 1500,
 					offset: $header.outerHeight()
 				});
+		// Animations
+		var $window = $(window);
+		var $elem = $(".animation")
+
+		    function isScrolledIntoView($elem, $window) {
+			var docViewTop = $window.scrollTop();
+			var docViewBottom = docViewTop + $window.height();
+
+			var elemTop = $elem.offset().top;
+			var elemBottom = elemTop + $elem.height();
+
+			return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+		    }
+
+
+		$(document).on("scroll", function () {
+		    if (isScrolledIntoView($elem, $window)) {
+			$elem.addClass("animate")
+			console.log("now you see me");
+		    }
+		});
 
 		// Menu.
 			$('#menu')
